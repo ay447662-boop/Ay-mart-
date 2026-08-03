e293e8028d99514abd3bb3473a10405e31303fef
Anil Yadav <ay447662@gmail.com>
2026-08-03 07:49:03 +0530

    Update firebase.js
    Signed-off-by: Anil Yadav <ay447662@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBc2UF6IRYS_TtBTLnoSuxqrAhjy2tYruk",
   authDomain: "aymart-4488a.firebaseapp.com",
+  databaseURL: "https://aymart-4488a-default-rtdb.firebaseio.com",
   projectId: "aymart-4488a",
   storageBucket: "aymart-4488a.firebasestorage.app",
   messagingSenderId: "325089901941",
@@ -13,8 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
 export { app, auth, db };
